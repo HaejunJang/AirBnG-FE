cf6891b914f00986430c73a30c27068841bf7d98
wocjs <friendly1217@naver.com>
2025-08-28 09:23:33 +0900

    chore : 백엔드 연결 관련 파일 셋업

--- airbng-frontend/src/api/http.js
+++ airbng-frontend/src/api/http.js
@@ -3,12 +3,19 @@ import axios from "axios";
 // .env 파일에 REACT_APP_API_BASE_URL 변수를 설정하여 API 기본 URL을 구성
 export const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
-export const http = axios.create({
+// 로그인 하지 않은 axios 인스턴스
+export const httpPublic = axios.create({
+    baseURL: API_BASE,
+    withCredentials: false,
+});
+
+// 로그인 한 axios 인스턴스
+export const httpAuth = axios.create({
     baseURL: API_BASE,
     withCredentials: true,
 });
 
-http.interceptors.request.use((config) => {
+httpAuth.interceptors.request.use((config) => {
     const token = localStorage.getItem("jwtToken");
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
