8dd3bd57a9c0eca784818cef1e78328710cc8b3f
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-08-28 19:43:36 +0900

    feat : navbar 가시 토글 및 예약 폼 라우트
    예약하기 화면에서는 navbar를 보여주지 않기 때문에
    가시 여부 선택 가능하도록 수정

--- airbng-frontend/src/App.js
+++ airbng-frontend/src/App.js
@@ -4,6 +4,7 @@ import "./styles/App.css";
 
 import ReservationDetail from "./pages/ReservationDetail";
 import HomePage from "./pages/HomePage";
+import ReservationFormPage from "./pages/ReservationFormPage";
 
 import "./styles/App.css";
 
@@ -22,11 +23,12 @@ function App() {
 
     // 네비바를 숨길 페이지들 정의
     const hideNavbarPaths = [
-      "/page/reservations/detail"
+      "/page/reservations/detail",
+      "/page/reservations/form",
     ];
-    
+
     // 현재 경로가 네비바를 숨겨야 하는 경로인지 확인
-    const shouldHideNavbar = hideNavbarPaths.some(path => 
+    const shouldHideNavbar = hideNavbarPaths.some((path) =>
       location.pathname.startsWith(path)
     );
 
@@ -35,7 +37,14 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/page/home" element={<HomePage />} />
-          <Route path="/page/reservations/detail/:id" element={<ReservationDetail />} />
+          <Route
+            path="/page/reservations/detail/:id"
+            element={<ReservationDetail />}
+          />
+          <Route
+            path="/page/reservations/form"
+            element={<ReservationFormPage />}
+          />
         </Routes>
         {!shouldHideNavbar && <Navbar active={active} />}
       </div>
