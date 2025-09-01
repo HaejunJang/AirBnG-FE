8aef2fcd3aca62a7602e27b86f2b7400a73b8b78
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-01 15:38:06 +0900

    fix : http.js 수정

--- airbng-frontend/src/api/reservationApi.js
+++ airbng-frontend/src/api/reservationApi.js
@@ -1,14 +1,16 @@
-import { http } from "./http";
+import { httpPublic } from "./http";
 
 // 예약 폼 가져오기
 export const getReservationForm = async (lockerId) => {
-  const response = await http.get("/reservations/form?lockerId=" + lockerId);
+  const response = await httpPublic.get(
+    "/reservations/form?lockerId=" + lockerId
+  );
   return response.data;
 };
 
 // 예약 post
 export const postReservation = async (req) => {
-  const response = await http.post("/reservations", req, {
+  const response = await httpPublic.post("/reservations", req, {
     headers: { "Content-Type": "application/json" },
   });
   return response.data;
