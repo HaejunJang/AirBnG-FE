6524951b76e450eb6aa1d960bb678facc91e141b
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-18 12:14:22 +0900

    feat : get 예약 폼 & post 예약 httpAuth로 변경

--- airbng-frontend/src/api/reservationApi.js
+++ airbng-frontend/src/api/reservationApi.js
@@ -1,51 +1,55 @@
 import qs from "qs";
 import { httpAuth, httpPublic } from "./http";
 
-export const getReservationList = ({ isDropper, memberId, state, nextCursorId, period }) => {
-    const safeState = state?.map(s => s.toUpperCase());
-    const safePeriod = period?.toUpperCase();
+export const getReservationList = ({
+  isDropper,
+  memberId,
+  state,
+  nextCursorId,
+  period,
+}) => {
+  const safeState = state?.map((s) => s.toUpperCase());
+  const safePeriod = period?.toUpperCase();
 
-    return httpAuth.get("/reservations", {
-        params: {
-            isDropper,
-            memberId,
-            state: safeState,
-            nextCursorId,
-            period: safePeriod,
-        },
-        paramsSerializer: params =>
-            qs.stringify(params, { arrayFormat: "repeat", skipNulls: true }),
-    });
+  return httpAuth.get("/reservations", {
+    params: {
+      isDropper,
+      memberId,
+      state: safeState,
+      nextCursorId,
+      period: safePeriod,
+    },
+    paramsSerializer: (params) =>
+      qs.stringify(params, { arrayFormat: "repeat", skipNulls: true }),
+  });
 };
 
 // 예약 취소
 export const cancelReservationApi = (reservationId, memberId) => {
-    return httpAuth.post(`/reservations/${reservationId}/members/${memberId}/cancel`);
+  return httpAuth.post(
+    `/reservations/${reservationId}/members/${memberId}/cancel`
+  );
 };
-
 
 // 예약 폼 가져오기
 export const getReservationForm = async (lockerId) => {
-    const response = await httpPublic.get(
-        "/reservations/form?lockerId=" + lockerId
-    );
-    return response.data;
+  const response = await httpAuth.get(
+    "/reservations/form?lockerId=" + lockerId
+  );
+  return response.data;
 };
 
 //예약 삭제
 export const deleteReservationApi = (reservationId) => {
-    return httpAuth.post(`/reservations/delete`, null, {
-        params: {reservationId},
-    });
-
-}
+  return httpAuth.post(`/reservations/delete`, null, {
+    params: { reservationId },
+  });
+};
 
 // 예약 post
-    export const postReservation = async (req) => {
-        const response = await httpPublic.post("/reservations", req, {
-            headers: {"Content-Type": "application/json"},
-        });
-        return response.data;
-    };
-
-
+export const postReservation = async (req) => {
+  const response = await httpAuth.post("/reservations", req, {
+    headers: { "Content-Type": "application/json" },
+  });
+  return response.data;
+};
